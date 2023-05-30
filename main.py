4bda3715beb34c24020819dae0742297cbca6894
doggbmx <manuelorticosta@gmail.com>
2023-05-30 18:53:47 -0400

    chupeting the flow

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import datetime as dt
 from sqlalchemy import extract
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler,ConversationHandler, filters
 
 # LOGGING CONFIGURATION
@@ -26,7 +26,6 @@ class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     event_description = db.Column(db.String(200), nullable=False)
     date = db.Column(db.DateTime, nullable=False)
-    reminded = db.Column(db.Boolean, nullable=False)
 
 TOKEN = open('token.txt', 'r').read()
 
@@ -39,52 +38,57 @@ final_date = None
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="What can I do for you today? /new to create a new reminder."
+        text="Que puedo hacer por ti hoy? /new to create a new reminder."
     )
     
 async def new_reminder(update, context):
-    await update.message.reply_text('What is the event description?')
+    await update.message.reply_text('Cual es la descripcion del evento que quieres guardar?')
     return 1
 
 async def get_description(update,context):
     global current_description
     current_description = update.message.text
-    await update.message.reply_text('What is the date? (dd-mm-yyyy)')
+    await update.message.reply_text('Que fecha? (dd/mm/yyyy) Te alertare varias veces al dia.')
     return 2
 
 async def get_date(update, context):
     global current_date
     global final_date
     current_date = update.message.text
-    print(f'current_date  {current_date}')
+    if (len(current_date) != 10) or (current_date[2] != '/') or (current_date[5] != '/'):
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='Formato de fecha invalido. Intentalo de vuelta con este formato => dd/mm/yyyy. Conversacion terminada.')
+        return ConversationHandler.END
     fecha = dt.datetime.strptime(current_date, '%d/%m/%Y').date()
-    print(f'fecha  {fecha}')
     format_date = fecha.strftime('%Y-%m-%d')
     slices = format_date.split('-')
     year = int(slices[0])
     month = int(slices[1])
     date = int(slices[2])
     final_date = dt.date(year=year, month=month, day=date)
-    print(f'format_date  {final_date}')
-    await update.message.reply_text(f'Are you sure you want to create the event? {current_description} on {current_date}? Reply with yes or no')
+    await update.message.reply_text(f'Estan correctos estos datos? \n'
+                                    f'{current_description} en fecha: {current_date}?', 
+                                    reply_markup=ReplyKeyboardMarkup([['Si', 'No']], 
+                                    one_time_keyboard=True)
+                                    )
     return 3
 
 async def confirm_event(update, context):
     global current_description
     global final_date
     global db
-    if update.message.text == 'yes':
+    print(update.message.from_user)
+    if update.message.text == 'Si' or update.message.text == 'si':
         with app.app_context():
             event_name = Event.query.filter_by(event_description=current_description).first()
         if not event_name:
             with app.app_context():
-                event = Event(event_description=current_description, date=final_date, reminded=False)
+                event = Event(event_description=current_description, date=final_date)
             with app.app_context():
                 db.session.add(event)
                 db.session.commit()
             context.job_queue.run_repeating(notify_event, interval=5, chat_id=update.message.chat_id)
         else:
-            await update.message.reply_text('Event already exists! Try again with /new.')
+            await update.message.reply_text('El evento ya existe! Prueba de vuelta con el comando /new.')
             return ConversationHandler.END
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
@@ -92,6 +96,7 @@ async def confirm_event(update, context):
         )
         return ConversationHandler.END
     else:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='Entendido! Cancelando operacion. Puedes agregar un nuevo evento con /new.')
         return ConversationHandler.END    
 
 def cancel():
@@ -101,23 +106,16 @@ async def notify_event(context):
     job = context.job
     today = dt.date.today()
     
-    # with app.app_context():
-    #     tests = Event.query.filter_by(reminded=False)
-    #     for test in tests:
-            
-            # print(f'test => {dt.datetime.strptime(test.date, "%Y-%m-%d") == dt.datetime.strptime(today, "%Y-%m-%d")}')
+   
     with app.app_context():
-        # event_today = Event.query.filter_by(Event.date.strftime('%Y-%m-%d') == today.strftime('%Y-%m-%d')).all()
         event_today = Event.query.filter(extract('day', Event.date)==today.day,
                                          extract('month', Event.date)==today.month,
-                                         extract('year', Event.date)==today.year).all()
-        print(f'event_today => {event_today}')
-        for event in event_today:
-            print('event today')
-    #         # context.bot.send_message('Event today: ' + event.event_description)
-    #         # event.reminded = True
-            await context.bot.send_message(job.chat_id, text=f'One event is about to start: {event.event_description}')
-    #         # db.session.commit()
+                                         extract('year', Event.date)==today.year,
+                                         ).all()
+        if event_today:
+            await context.bot.send_message(job.chat_id, text=f'Buenas bb! Hoy tienes los siguientes eventos:')
+            for event in event_today:
+                await context.bot.send_message(job.chat_id, text=f'{event.event_description}')
 
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
@@ -125,29 +123,12 @@ async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
         text="Sorry, I didn't understand that command."
         )
 
-## DEPRECATED
-def check_events():
-    while True:
-        today = dt.date.today()
-        print(today)
-        with app.app_context():
-            event_today = Event.query.filter_by(reminded=False).filter(Event.date == today).all()
-            print(f'event_today => {event_today}')
-            for event in event_today:
-                # context.bot.send_message('Event today: ' + event.event_description)
-                event.reminded = True
-                db.session.commit()
-                
-        time.sleep(60)
-        
-# threading.Thread(target=check_events).start()
-
 conv_handler = ConversationHandler(
     entry_points=[CommandHandler('new', new_reminder)],
     states={
         1: [MessageHandler(filters.TEXT, get_description)],
         2: [MessageHandler(filters.TEXT, get_date)],
-        3: [MessageHandler(filters.TEXT, confirm_event)],
+        3: [MessageHandler(filters.Regex("^(Si|No|si|no)$"), confirm_event)],
     },
     fallbacks=[CommandHandler('cancel', cancel)],
     conversation_timeout=120,
@@ -155,6 +136,9 @@ conv_handler = ConversationHandler(
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        
     application = ApplicationBuilder().token(TOKEN).build()
     
     start_handler = CommandHandler('start', start)
